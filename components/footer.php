9bdae21a58867261324b6f38b81312bdf62bf276
SushmithaAcharya2004 <sushmiacharya2004@gmail.com>
2024-10-12 21:55:31 +0530

    This is my complete project submission

---
+++ components/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href=""><i class="fas fa-phone"></i> +91 000 000 0000</a>
+         <a href=""><i class="fas fa-phone"></i> +91 000 000 0000</a>
+         <a href="mailto:sushmiacharya2004@gmail.com"><i class="fas fa-envelope"></i> sushmitha@gmail.com</a>
+         <a href=""><i class="fas fa-map-marker-alt"></i> Coimbatore, India </a>
+      </div>
+
+      
+
+   </section>
+
+   
+
+</footer>
